36392544883791eb284e062bc8f971651d0f9e4d
James Corfe <james.corfe@gmail.com>
2023-10-16 17:30:25 +0100

    code update - React

--- interface-react/src/App.js
+++ interface-react/src/App.js
@@ -1,29 +1,12 @@
-import logo from './logo.svg';
+// App.js
+import React from 'react';
 import './App.css';
-
-
+import RecipeList from './RecipeList'; // Import the RecipeList component
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div>
-          <h1>Hello!</h1>
-          <p>Welcome to your React app.</p>
-        </div>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RecipeList />
     </div>
   );
 }
